841863a4777c94a4be49ff06d2cc5d86a25f00ef
unknown <Administrator@MEDICITY.CO.IN>
2022-11-24 14:46:58 +0530

    Diet Extra Oder view and update Completed

--- api/dietextraorder/extraorder.router.js
+++ api/dietextraorder/extraorder.router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { checkToken } = require("../../authentication/token_validation");
 const { getExtraorder, getItemrate, ExtraOrderInsert, ExtraOrderListInsert, getDietType,
-    getExtraOrder, getExtraOrderDetail } = require('../dietextraorder/extraorder.controller');
+    getExtraOrder, getExtraOrderDetail, updateExta } = require('../dietextraorder/extraorder.controller');
 
 router.post("/", checkToken, getExtraorder);
 router.post("/rate", checkToken, getItemrate);
@@ -10,4 +10,5 @@ router.post("/insertextra", checkToken, ExtraOrderListInsert);
 router.post("/dietType/get", checkToken, getDietType)
 router.get('/getExtraOrder', checkToken, getExtraOrder)
 router.get("/getExtraOrderDetail/:id", checkToken, getExtraOrderDetail)
+router.patch("/updateExta", checkToken, updateExta)
 module.exports = router;

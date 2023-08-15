dd424fb2d78c766f25f62c6a6ab4477bd3fd164f
unknown <Administrator@MEDICITY.CO.IN>
2023-08-15 13:37:59 +0530

    Dashboard for Room management

---
+++ api/rm_dashboard/dashboard.services.js
@@ -0,0 +1,69 @@
+const { pool } = require('../../config/database')
+module.exports = {
+    floorDashgetById: (id, callback) => {
+        pool.query(
+            `select rm_floor_slno,rm_floor_name
+            from rm_floor_creation            
+            where rm_floor_building_slno=?`,
+            [id],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+        );
+    },
+
+    RoomDashgetById: (id, callback) => {
+        pool.query(
+            `select rm_room_slno,rm_room_name
+            from rm_newroom_creation            
+            where rm_room_floor_slno=?`,
+            [id],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+        );
+    },
+    FloorNDroomById: (id, callback) => {
+        pool.query(
+
+            `SELECT 
+            rm_room_slno, rm_room_name,rm_room_no, rm_room_alias,
+            rm_newroom_creation.rm_build_slno,
+            rm_newroom_creation.rm_room_floor_slno,
+            rm_newroom_creation.rm_insidebuilldblock_slno, 
+            rm_newroom_creation.rm_roomtype_slno,
+            rm_newroom_creation.rm_category_slno, 
+            rm_room_status,
+            rm_building_mast.rm_building_name,			
+            rm_floor_creation.rm_floor_name,
+            rm_insidebuildblock_mast.rm_insidebuildblock_name,
+            rm_room_type_master.rm_roomtype_name,
+            rm_room_category_master.rm_roomcategory_name 
+            from 
+            rm_newroom_creation
+            LEFT JOIN rm_building_mast ON rm_building_mast.rm_building_slno=rm_newroom_creation.rm_build_slno    
+            LEFT JOIN rm_floor_creation ON rm_floor_creation.rm_floor_slno=rm_newroom_creation.rm_room_floor_slno          
+            LEFT JOIN rm_insidebuildblock_mast ON rm_insidebuildblock_mast.rm_insidebuildblock_slno=rm_newroom_creation.rm_insidebuilldblock_slno
+            LEFT JOIN rm_room_type_master ON rm_room_type_master.rm_roomtype_slno=rm_newroom_creation.rm_roomtype_slno
+            LEFT JOIN rm_room_category_master ON rm_room_category_master.rm_roomcategory_slno=rm_newroom_creation.rm_category_slno
+            where rm_room_floor_slno=?`,
+            [id],
+
+            (error, results, fields) => {
+
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+
+        );
+    },
+
+}

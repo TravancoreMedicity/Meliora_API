4f574d966eadad948a802d129dcb212bfa35f2be
unknown <Administrator@MEDICITY.CO.IN>
2023-11-15 18:19:16 +0530

    Correction in asset creation and opening entry

--- api/am_Item_creation_mast/item_creation_mast.service.js
+++ api/am_Item_creation_mast/item_creation_mast.service.js
@@ -290,6 +290,64 @@ module.exports = {
         )
     },
 
+    getSpareItemsFronListonlydept: (data, callBack) => {
+        pool.query(
+            `SELECT 
+            am_spare_item_map_master.am_spare_item_map_slno,  am_spare_item_map_master.spare_creation_slno,spare_dept_slno,spare_deptsec_slno,
+            co_department_mast.dept_name as deptname,co_deptsec_mast.sec_name as secname,spare_custodian_dept,
+            am_custodian_name,
+            am_item_name_creation.item_name,spare_asset_no,spare_asset_no_only,due_date
+          FROM
+          am_spare_item_map_master
+         left join co_department_mast on co_department_mast.dept_id=am_spare_item_map_master.spare_dept_slno
+          left join co_deptsec_mast on co_deptsec_mast.sec_id=am_spare_item_map_master.spare_deptsec_slno
+           left join am_item_name_creation on am_item_name_creation.item_creation_slno=am_spare_item_map_master.spare_creation_slno
+           left join am_custodian_department on am_custodian_department.am_custodian_slno=am_spare_item_map_master.spare_custodian_dept
+           left join am_item_map_amcpm_detail on am_item_map_amcpm_detail.am_item_map_slno=am_spare_item_map_master.am_spare_item_map_slno
+          WHERE
+           spare_dept_slno = ? and spare_create_status=1
+           ORDER BY am_spare_item_map_master.am_spare_item_map_slno DESC`,
+            [
+                data.spare_dept_slno
+            ],
+            (error, results, feilds) => {
+                if (error) {
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        )
+    },
+    getSpareItemsFronListdeptandsec: (data, callBack) => {
+        pool.query(
+            `SELECT 
+            am_spare_item_map_master.am_spare_item_map_slno,  am_spare_item_map_master.spare_creation_slno,spare_dept_slno,spare_deptsec_slno,
+            co_department_mast.dept_name as deptname,co_deptsec_mast.sec_name as secname,spare_custodian_dept,
+            am_custodian_name,
+            am_item_name_creation.item_name,spare_asset_no,spare_asset_no_only,due_date
+          FROM
+          am_spare_item_map_master
+         left join co_department_mast on co_department_mast.dept_id=am_spare_item_map_master.spare_dept_slno
+          left join co_deptsec_mast on co_deptsec_mast.sec_id=am_spare_item_map_master.spare_deptsec_slno
+           left join am_item_name_creation on am_item_name_creation.item_creation_slno=am_spare_item_map_master.spare_creation_slno
+           left join am_custodian_department on am_custodian_department.am_custodian_slno=am_spare_item_map_master.spare_custodian_dept
+           left join am_item_map_amcpm_detail on am_item_map_amcpm_detail.am_item_map_slno=am_spare_item_map_master.am_spare_item_map_slno
+          WHERE
+           spare_dept_slno = ?
+           and spare_deptsec_slno=?  and spare_create_status=1
+           ORDER BY am_spare_item_map_master.am_spare_item_map_slno DESC`,
+            [
+                data.spare_dept_slno,
+                data.spare_deptsec_slno
+            ],
+            (error, results, feilds) => {
+                if (error) {
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        )
+    },
     getSpareItemsFronList: (data, callBack) => {
         pool.query(
             `SELECT 
@@ -313,6 +371,66 @@ module.exports = {
                 data.spare_dept_slno,
                 data.spare_deptsec_slno,
                 data.spare_creation_slno
+            ],
+            (error, results, feilds) => {
+                if (error) {
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        )
+    },
+
+    getItemsFronListonlydept: (data, callBack) => {
+        pool.query(
+            `SELECT 
+            am_asset_item_map_master.am_item_map_slno,  am_asset_item_map_master.item_creation_slno,item_dept_slno,item_deptsec_slno,
+            co_department_mast.dept_name as deptname,co_deptsec_mast.sec_name as secname,item_custodian_dept,
+            am_custodian_name,
+            am_item_name_creation.item_name,item_asset_no,item_asset_no_only,due_date
+          FROM
+          am_asset_item_map_master
+         left join co_department_mast on co_department_mast.dept_id=am_asset_item_map_master.item_dept_slno
+          left join co_deptsec_mast on co_deptsec_mast.sec_id=am_asset_item_map_master.item_deptsec_slno
+           left join am_item_name_creation on am_item_name_creation.item_creation_slno=am_asset_item_map_master.item_creation_slno
+           left join am_custodian_department on am_custodian_department.am_custodian_slno=am_asset_item_map_master.item_custodian_dept
+           left join am_item_map_amcpm_detail on am_item_map_amcpm_detail.am_item_map_slno=am_asset_item_map_master.am_item_map_slno
+          WHERE
+           item_dept_slno = ?
+           and  item_create_status=1
+           ORDER BY am_asset_item_map_master.am_item_map_slno DESC`,
+            [
+                data.item_dept_slno
+            ],
+            (error, results, feilds) => {
+                if (error) {
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        )
+    },
+    getItemsFronListdeptandsec: (data, callBack) => {
+        pool.query(
+            `SELECT 
+            am_asset_item_map_master.am_item_map_slno,  am_asset_item_map_master.item_creation_slno,item_dept_slno,item_deptsec_slno,
+            co_department_mast.dept_name as deptname,co_deptsec_mast.sec_name as secname,item_custodian_dept,
+            am_custodian_name,
+            am_item_name_creation.item_name,item_asset_no,item_asset_no_only,due_date
+          FROM
+          am_asset_item_map_master
+         left join co_department_mast on co_department_mast.dept_id=am_asset_item_map_master.item_dept_slno
+          left join co_deptsec_mast on co_deptsec_mast.sec_id=am_asset_item_map_master.item_deptsec_slno
+           left join am_item_name_creation on am_item_name_creation.item_creation_slno=am_asset_item_map_master.item_creation_slno
+           left join am_custodian_department on am_custodian_department.am_custodian_slno=am_asset_item_map_master.item_custodian_dept
+           left join am_item_map_amcpm_detail on am_item_map_amcpm_detail.am_item_map_slno=am_asset_item_map_master.am_item_map_slno
+          WHERE
+           item_dept_slno = ?
+           and item_deptsec_slno=? and item_create_status=1
+           ORDER BY am_asset_item_map_master.am_item_map_slno DESC`,
+            [
+                data.item_dept_slno,
+                data.item_deptsec_slno
             ],
             (error, results, feilds) => {
                 if (error) {
--- api/am_item_name_creation/item.router.js
+++ api/am_item_name_creation/item.router.js
@@ -1,15 +1,14 @@
 const router = require("express").Router();
 const { checkToken } = require("../../authentication/token_validation");
 const { ItemNameInsert, ItemNameview, ItemNameUpdate,
-    getitemFromMaster, getitemFromMasterdemo
+    getitemFromMaster, getitemFromMasterdemo, getItemSearchByName
 } = require('../am_item_name_creation/item.controller');
 
 router.post('/insert', checkToken, ItemNameInsert)
 router.get('/view', checkToken, ItemNameview)
 router.patch('/update', checkToken, ItemNameUpdate)
-
 router.post('/getItem', checkToken, getitemFromMaster)
-
 router.post('/getItemdemo', checkToken, getitemFromMasterdemo)
 
+router.post('/getItemByName', checkToken, getItemSearchByName)
 module.exports = router

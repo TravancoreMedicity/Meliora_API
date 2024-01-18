f00f95d0fd90b4775130da1cf87b7f2fd9a40bf6
unknown <Administrator@MEDICITY.CO.IN>
2024-01-18 17:10:45 +0530

    Image upload corrections in task management

--- api/tm_task_file_upload/task_file_upload.router.js
+++ api/tm_task_file_upload/task_file_upload.router.js
@@ -1,17 +1,11 @@
 const router = require('express').Router();
 const { checkToken } = require("../../authentication/token_validation");
-const { uploadFileTask, getTaskFile, uploadFileSubTask, getSubTaskFile } = require('../tm_task_file_upload/task_file_upload.controller')
+const { uploadFileTask, getTaskFile, } = require('../tm_task_file_upload/task_file_upload.controller')
 
 
 
 router.post("/uploadFile/task", checkToken, uploadFileTask)
 router.get("/uploadFile/getTaskFile/:id", getTaskFile)
-
-
-
-
-router.post("/uploadFile/subtask", checkToken, uploadFileSubTask)
-router.get("/uploadFile/getSubTaskFile/:id", getSubTaskFile)
 
 
 module.exports = router;

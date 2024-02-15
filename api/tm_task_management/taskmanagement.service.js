947aa7e91b77f037860ed99680f81f0a1c283180
unknown <Administrator@MEDICITY.CO.IN>
2024-02-15 14:47:55 +0530

    Task management for live

--- api/tm_task_management/taskmanagement.service.js
+++ api/tm_task_management/taskmanagement.service.js
@@ -140,7 +140,8 @@ module.exports = {
 			tm_task_due_date, 
             tm_assigne_emp,
             tm_detail_status,
-            co_employee_master.em_name,           
+            co_employee_master.em_name,
+            tm_new_task_mast.create_date,         
             main_task_slno,
             tm_new_task_mast.tm_project_slno,
 			tm_task_description,
@@ -328,7 +329,7 @@ module.exports = {
             left join tm_project_mast on tm_project_mast.tm_project_slno=tm_new_task_mast.tm_project_slno 
             left join tm_new_task_mast_detl on tm_new_task_mast_detl.tm_task_slno=tm_new_task_mast.tm_task_slno
             left join co_employee_master on co_employee_master.em_id=tm_new_task_mast_detl.tm_assigne_emp
-            WHERE tm_new_task_mast.tm_task_dept_sec=?
+            WHERE ((tm_new_task_mast.tm_task_dept_sec=?) And (tm_new_task_mast.tm_task_status!=1))
             group by tm_new_task_mast.tm_task_slno
 			ORDER BY tm_task_slno DESC`,
             [id],

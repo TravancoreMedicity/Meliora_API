cd5f3c2f4ce0cdad4732b5c987ed9ca0117452ae
unknown <Administrator@MEDICITY.CO.IN>
2024-04-23 16:09:35 +0530

    In store menu list query correction

--- api/crm_new_approvals/newReqApprovalsList.controller.js
+++ api/crm_new_approvals/newReqApprovalsList.controller.js
@@ -1,4 +1,5 @@
-const { getClosedReqList, getAllReqListNotAck, getMOAppvalPending
+const { getClosedReqList, getAllReqListNotAck, getMOAppvalPending, getSMOAppvalPending,
+    getGMAppvalPending
 } = require('../crm_new_approvals/newReqApprovalsList.service');
 
 const logger = require('../../logger/logger');
@@ -66,9 +67,47 @@ module.exports = {
             });
         });
     },
+    getSMOAppvalPending: (req, res) => {
+        getSMOAppvalPending((err, results) => {
+            if (err) {
+                return res.status(200).json({
+                    success: 2,
+                    message: err
+                });
+            }
+            if (!results) {
+                return res.status(200).json({
+                    success: 0,
+                    message: "No results found"
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
+        });
+    },
 
-
-
+    getGMAppvalPending: (req, res) => {
+        getGMAppvalPending((err, results) => {
+            if (err) {
+                return res.status(200).json({
+                    success: 2,
+                    message: err
+                });
+            }
+            if (!results) {
+                return res.status(200).json({
+                    success: 0,
+                    message: "No results found"
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
+        });
+    },
 
 }
 

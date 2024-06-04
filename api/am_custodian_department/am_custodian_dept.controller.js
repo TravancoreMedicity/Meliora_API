0022f4b725c8ba43bdcb5b57b9b7e4ee75ae96d9
unknown <Administrator@MEDICITY.CO.IN>
2024-06-04 11:33:30 +0530

    Spare transfer to condemnation and service page done

--- api/am_custodian_department/am_custodian_dept.controller.js
+++ api/am_custodian_department/am_custodian_dept.controller.js
@@ -1,5 +1,6 @@
 const { validateAssetType } = require('../../validation/validation_schema');
-const { CustodianDeptInsert, CustodianDepView, CustodianDepUpdate, CustodianDepSelect
+const { CustodianDeptInsert, CustodianDepView, CustodianDepUpdate, CustodianDepSelect,
+    selectById
 } = require('../am_custodian_department/am_custodian_dept.services')
 module.exports = {
     CustodianDeptInsert: (req, res) => {
@@ -99,4 +100,29 @@ module.exports = {
 
         })
     },
+
+    selectById: (req, res) => {
+        const id = req.params.id;
+        selectById(id, (err, results) => {
+            if (err) {
+                logger.logwindow(err)
+                return res.status(400).json({
+                    success: 0,
+                    message: err
+                });
+            }
+            if (results.length === 0) {
+                return res.status(200).json({
+                    success: 2,
+                    message: "No Record Found"
+                });
+            }
+
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+
 }
--- api/employee/employee.controller.js
+++ api/employee/employee.controller.js
@@ -188,6 +188,7 @@ module.exports = {
                     emp_secid: results.em_dept_section,
                     app_token: results.app_token,
                     emp_dept: results.em_department,
+                    dept_name: results.dept_name,
                     logintime: results.login,
                     supervisor: results.supervisor,
                     logOutTime: format(addHours(new Date(results.login), logout_time), 'yyyy-MM-dd HH:mm:ss')

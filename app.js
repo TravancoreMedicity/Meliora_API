6bf69f3ddc4b1da4013bda82590b56d07b34c1bc
itmedicity <itmedicity@tmc.ac.in>
2025-05-05 15:29:30 +0530

    core changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('./logger/logger');
 const http = require("http");
 const socketUtils = require('./socketio/socketUltil');
 const cookieParser = require('cookie-parser');
-const lusca = require('lusca')
+// const lusca = require('lusca')
 
 const app = express();
 const fs = require('fs');
@@ -16,11 +16,11 @@ const https = require('https');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(lusca.csrf());
+// app.use(lusca.csrf());
 
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://192.168.22.8:3000'],
+    origin: ['http://localhost:3000', 'http://192.168.10.61:3000'],
     credentials: true
 }));
 

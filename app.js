2c9bb6eb375bd5ceeff1b1778afa2b8e84b7990b
Ajiminsha <ajiminsha@tmc.ac.in>
2025-05-08 11:10:47 +0530

    Ticket Module Updations

--- app.js
+++ app.js
@@ -28,8 +28,9 @@ app.use(cors({
         'http://travancoremedicity.in:9741',
         'http://192.168.10.88:3000',
         'http://localhost:3000',
-        ' http://tm.medicity.co.in:8888',
-        ' http://192.168.10.88:8888',
+        'http://tm.medicity.co.in:8888',
+        'http://192.168.10.88:8888',
+        'http://192.168.22.9:3000'
         ,
     ],
     credentials: true

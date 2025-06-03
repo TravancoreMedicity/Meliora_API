8b7927fcc9c862a9fb425712373821dc94f224d3
unknown <Administrator@MEDICITY.CO.IN>
2025-06-03 11:44:47 +0530

    changes in app

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use(
             'https://travancoremedicity.in:9742',
             'http://travancoremedicity.in:9741',
             'http://192.168.10.88:3000',
-            'http://localhost:3000',
             ' http://tm.medicity.co.in:8888',
             ' http://192.168.10.88:8888',
             'http://localhost:3002', ,

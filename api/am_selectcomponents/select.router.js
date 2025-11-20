73d6a5d7572e664fa18b168fec6c0297f9c4a6c6
Ajiminsha <ajiminsha@tmc.ac.in>
2025-11-20 10:37:22 +0530

    Item Wise Search asset

--- api/am_selectcomponents/select.router.js
+++ api/am_selectcomponents/select.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { checkToken } = require("../../authentication/token_validation");
 const { getCategory, getGroup, getAssetType, getAmItemType, getAmSubcategory, getAmSubGroup, getAmManufacture,
     getAmModel, getUOM, getSubmodel, modelNoSelect, ItemBasedOnDeptSec, SpareItemBasedOnDeptSec, rackselect,
-    RoomBasedOnDeptSec, SubRoomunderRoom
+    RoomBasedOnDeptSec, SubRoomunderRoom,AssetItemUnderCustoidian,SpareItemUnderCustoidian
 } = require('../am_selectcomponents/select.controller');
 router.get('/categoryDropdown', checkToken, getCategory)
 router.get('/groupDropdown', checkToken, getGroup)
@@ -20,4 +20,7 @@ router.get('/SpareItemBasedOnDeptSec/:id', checkToken, SpareItemBasedOnDeptSec)
 router.get('/rackselect', checkToken, rackselect)
 router.get('/RoomBasedOnDeptSec/:id', checkToken, RoomBasedOnDeptSec)
 router.get('/SubRoomunderRoom/:id', checkToken, SubRoomunderRoom)
+
+router.get('/AssetItemUnderCustoidian/:id', checkToken, AssetItemUnderCustoidian)
+router.get('/SpareItemUnderCustoidian/:id', checkToken, SpareItemUnderCustoidian)
 module.exports = router;

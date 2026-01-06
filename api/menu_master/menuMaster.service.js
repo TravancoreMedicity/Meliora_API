c51c4033164b04c4db8a9fa917ced19ead966ed3
unknown <Administrator@MEDICITY.CO.IN>
2026-01-06 10:05:30 +0530

    Created Menu Master

--- api/menu_master/menuMaster.service.js
+++ api/menu_master/menuMaster.service.js
@@ -6,14 +6,17 @@ module.exports = {
             `INSERT INTO menu_master (
                 menu_name,
                 menu_module_slno,
-                menu_status
+                sub_module_slno,
+                menu_status,
+                create_user
             )
-            VALUES (?,?,?)`,
+            VALUES (?,?,?,?,?)`,
             [
                 data.menu_name,
                 data.menu_module_slno,
-                // JSON.stringify(data.menu_module_slno),
-                data.menu_status
+                data.sub_module_slno,
+                data.menu_status,
+                data.user
             ],
             (error, results, feilds) => {
                 if (error) {
@@ -28,9 +31,11 @@ module.exports = {
             `SELECT menu_name,
             menu_status
             FROM menu_master
-            WHERE menu_name=? `,
+            WHERE menu_name= ? and menu_module_slno=? and sub_module_slno = ?`,
             [
-                data.menu_name
+                data.menu_name,
+                data.menu_module_slno,
+                data.sub_module_slno
             ],
             (error, results, feilds) => {
                 if (error) {
@@ -63,12 +68,16 @@ module.exports = {
             `UPDATE menu_master 
                 SET menu_name = ?,
                 menu_module_slno =?,
-                menu_status=?
+                sub_module_slno =?,
+                menu_status=?,
+                edit_user=?
                 WHERE menu_slno = ?`,
             [
                 data.menu_name,
-                JSON.stringify(data.menu_module_slno),
+                data.menu_module_slno,
+                data.sub_module_slno,
                 data.menu_status,
+                data.edit_user,
                 data.menu_slno
             ],
             (error, results, feilds) => {
@@ -82,10 +91,17 @@ module.exports = {
     getSelect: (callBack) => {
         pool.query(
             `SELECT 
-            menu_slno,
-            menu_name,
-            menu_module_slno
-            FROM menu_master `,
+                mm.menu_slno,
+                mm.menu_name,
+                menu_module_slno,
+                mm.sub_module_slno,
+                mm.menu_status,
+                mdm.module_name,
+                sm.sub_module_name
+            FROM
+                menu_master mm
+            LEFT JOIN module_master mdm on mdm.module_slno = mm.menu_module_slno
+            LEFT JOIN sub_modules sm on sm.sub_module_slno = mm.sub_module_slno`,
             [],
             (error, results, feilds) => {
                 if (error) {
